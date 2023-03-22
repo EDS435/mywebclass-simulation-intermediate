ae98ead47cff7790ef7ff42872e180068d26ab7b
jgwentworth92 <95505687+jgwentworth92@users.noreply.github.com>
2023-03-22 19:06:09 -0400

    screenshot and video should be saved to test result folder on failed tests

--- playwright.config.js
+++ playwright.config.js
@@ -37,7 +37,7 @@ module.exports = defineConfig({
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
-    video: 'on',
+      video: 'on', screenshot: 'on',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry'
   },
@@ -49,7 +49,7 @@ module.exports = defineConfig({
       use: { ...devices['Desktop Chrome'] }
     }
 
-    /* Test against mobile viewports. */
+    
     // {
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
@@ -70,8 +70,8 @@ module.exports = defineConfig({
     // },
   ],
 
-  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
+  
+  outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
   webServer: {
