e5acea65c1591d97fe4f3b7d708a8b92c8852938
jgwentworth92 <95505687+jgwentworth92@users.noreply.github.com>
2023-03-22 22:37:25 -0400

    lighthouse should produce report if tests fail, only produces on success. maybe extended timeout fix things

--- tests/lighthouse.test.js
+++ tests/lighthouse.test.js
@@ -7,7 +7,7 @@ test('Lighthouse audit home page', async ({ page }) => {
 
   const url = page.url();
   const reportPath = 'lighthouse-report.json';
-  const command = `lighthouse ${url} --output json --output-path ${reportPath} --only-categories=performance`;
+  const command = `npx lighthouse ${url} --output json --output-path ${reportPath} --only-categories=performance`;
 
   try {
     execSync(command);
@@ -21,63 +21,59 @@ test('Lighthouse audit home page', async ({ page }) => {
   expect(performanceScore).toBeGreaterThanOrEqual(85);
 });
 
-
 test('Lighthouse audit home page for accessibility', async ({ page }) => {
-    await page.goto('http://localhost:3000');
-  
-    const url = page.url();
-    const reportPath = 'lighthouse-report.json';
-    const command = `lighthouse ${url} --output json --output-path ${reportPath} --only-categories=accessibility`;
-  
-    try {
-      execSync(command);
-    } catch (error) {
-      console.error('Error running Lighthouse:', error);
-    }
-  
-    const lhr = JSON.parse(readFileSync(reportPath));
-  
-    const accessibilityScore = lhr.categories.accessibility.score * 100;
-    expect(accessibilityScore).toBeGreaterThanOrEqual(80);
-  });
-  
-  test('Lighthouse audit content page for accessibility', async ({ page }) => {
-    await page.goto('http://localhost:3000/content.html');
-  
-    const url = page.url();
-    const reportPath = 'lighthouse-report.json';
-    const command = `lighthouse ${url} --output json --output-path ${reportPath} --only-categories=accessibility`;
-  
-    try {
-      execSync(command);
-    } catch (error) {
-      console.error('Error running Lighthouse:', error);
-    }
-  
-    const lhr = JSON.parse(readFileSync(reportPath));
-  
-    const accessibilityScore = lhr.categories.accessibility.score * 100;
-    expect(accessibilityScore).toBeGreaterThanOrEqual(90);
-  });
+  await page.goto('http://localhost:3000');
 
+  const url = page.url();
+  const reportPath = 'lighthouse-report.json';
+  const command = `npx lighthouse ${url} --output json --output-path ${reportPath} --only-categories=accessibility`;
+
+  try {
+    execSync(command);
+  } catch (error) {
+    console.error('Error running Lighthouse:', error);
+  }
+
+  const lhr = JSON.parse(readFileSync(reportPath));
+
+  const accessibilityScore = lhr.categories.accessibility.score * 100;
+  expect(accessibilityScore).toBeGreaterThanOrEqual(80);
+});
+
+test('Lighthouse audit content page for accessibility', async ({ page }) => {
+  await page.goto('http://localhost:3000/content.html');
+
+  const url = page.url();
+  const reportPath = 'lighthouse-report.json';
+  const command = `npx lighthouse ${url} --output json --output-path ${reportPath} --only-categories=accessibility`;
+
+  try {
+    execSync(command);
+  } catch (error) {
+    console.error('Error running Lighthouse:', error);
+  }
+
+  const lhr = JSON.parse(readFileSync(reportPath));
+
+  const accessibilityScore = lhr.categories.accessibility.score * 100;
+  expect(accessibilityScore).toBeGreaterThanOrEqual(90);
+});
 
 test('Lighthouse audit content page', async ({ page }) => {
-    await page.goto('http://localhost:3000/content.html');
-  
-    const url = page.url();
-    const reportPath = 'lighthouse-report.json';
-    const command = `lighthouse ${url} --output json --output-path ${reportPath} --only-categories=performance`;
-  
-    try {
-      execSync(command);
-    } catch (error) {
-      console.error('Error running Lighthouse:', error);
-    }
-  
-    const lhr = JSON.parse(readFileSync(reportPath));
-  
-    const performanceScore = lhr.categories.performance.score * 100;
-    expect(performanceScore).toBeGreaterThanOrEqual(90);
-  });
+  await page.goto('http://localhost:3000/content.html');
 
+  const url = page.url();
+  const reportPath = 'lighthouse-report.json';
+  const command = `npx lighthouse ${url} --output json --output-path ${reportPath} --only-categories=performance`;
 
+  try {
+    execSync(command);
+  } catch (error) {
+    console.error('Error running Lighthouse:', error);
+  }
+
+  const lhr = JSON.parse(readFileSync(reportPath));
+
+  const performanceScore = lhr.categories.performance.score * 100;
+  expect(performanceScore).toBeGreaterThanOrEqual(90);
+});
