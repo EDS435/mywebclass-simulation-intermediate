6f385989976935cfe5e4658760be3289d70f0a8f
jgwentworth92 <95505687+jgwentworth92@users.noreply.github.com>
2023-03-26 17:37:40 -0400

    added lighthouse seo and best practice tests

--- tests/lighthouse.test.js
+++ tests/lighthouse.test.js
@@ -77,3 +77,93 @@ test('Lighthouse audit content page', async ({ page }) => {
   const performanceScore = lhr.categories.performance.score * 100;
   expect(performanceScore).toBeGreaterThanOrEqual(90);
 });
+
+
+test('Lighthouse content page audit SEO ', async ({ page }) => {
+  await page.goto('http://localhost:3000/content.html');
+
+  const url = page.url();
+  const reportPath = 'lighthouse-report.json';
+  const command = `npx lighthouse ${url} --output json --output-path ${reportPath} --only-categories=seo,best-practices`;
+
+  try {
+    execSync(command);
+  } catch (error) {
+    console.error('Error running Lighthouse:', error);
+  }
+
+  const lhr = JSON.parse(readFileSync(reportPath));
+
+  const seoScore = lhr.categories.seo.score * 100;
+
+  expect(seoScore).toBeGreaterThanOrEqual(90);
+
+});
+
+
+test('Lighthouse homepage page audit SEO ', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  const url = page.url();
+  const reportPath = 'lighthouse-report.json';
+  const command = `npx lighthouse ${url} --output json --output-path ${reportPath} --only-categories=seo,best-practices`;
+
+  try {
+    execSync(command);
+  } catch (error) {
+    console.error('Error running Lighthouse:', error);
+  }
+
+  const lhr = JSON.parse(readFileSync(reportPath));
+
+  const seoScore = lhr.categories.seo.score * 100;
+
+
+  expect(seoScore).toBeGreaterThanOrEqual(90);
+
+});
+
+
+
+
+test('Lighthouse content page audit Best Practices', async ({ page }) => {
+  await page.goto('http://localhost:3000/content.html');
+
+  const url = page.url();
+  const reportPath = 'lighthouse-report.json';
+  const command = `npx lighthouse ${url} --output json --output-path ${reportPath} --only-categories=seo,best-practices`;
+
+  try {
+    execSync(command);
+  } catch (error) {
+    console.error('Error running Lighthouse:', error);
+  }
+
+  const lhr = JSON.parse(readFileSync(reportPath));
+
+  
+  const bestPracticesScore = lhr.categories['best-practices'].score * 100;
+
+  expect(bestPracticesScore).toBeGreaterThanOrEqual(90);
+});
+
+test('Lighthouse homepage audit Best Practices', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  const url = page.url();
+  const reportPath = 'lighthouse-report.json';
+  const command = `npx lighthouse ${url} --output json --output-path ${reportPath} --only-categories=seo,best-practices`;
+
+  try {
+    execSync(command);
+  } catch (error) {
+    console.error('Error running Lighthouse:', error);
+  }
+
+  const lhr = JSON.parse(readFileSync(reportPath));
+
+  
+  const bestPracticesScore = lhr.categories['best-practices'].score * 100;
+
+  expect(bestPracticesScore).toBeGreaterThanOrEqual(90);
+});
