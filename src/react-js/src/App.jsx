044a7009997e31b61b18b922d67b3d6c8bc1c610
Emmanuel Chiobi <ec362@njit.edu>
2023-03-27 19:22:48 -0400

    Final edits on React

--- src/react-js/src/App.jsx
+++ src/react-js/src/App.jsx
@@ -3,8 +3,6 @@ import Header from './components/header';
 import Content from './components/body';
 import Footer from './components/footer';
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
